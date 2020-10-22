e4d19ea98e692bd4b10f1ed8e53180c255fd812c
m00-git <c0wcl4n@outlook.com>
2020-10-21 22:28:10 -0400

    Create scrap.py

---
+++ scrap.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import discord
+from discord.ext import commands
+
+TOKEN_AUTH = "TOKENHERE"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as '+client.user.name+'')
+
+@commands.command()
+async def getallmembers(self, ctx):
+ print("\n===", ctx.guild.name, "===")
+ print("Dumping all members to file...")
+ with open(ctx.guild.name+"_members.txt", "w") as f:
+   users = '\n'.join(str(user) for user in ctx.guild.members)
+   f.write(users)
+
+client.add_command(getallmembers) #or something shit idk
+
+client.run(TOKEN_AUTH, bot=False)
